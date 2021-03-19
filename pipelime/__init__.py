494ed3daaf1931eac5fca5974b0807a4741a5ad1
degregorio.daniele@gmail.com <degregorio.daniele@gmail.com>
2021-03-19 23:46:58 +0100

    Bump version: 0.0.1 → 0.1.0

--- pipelime/__init__.py
+++ pipelime/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = "eyecan"
 __email__ = 'daniele.degregorio@eyecan.ai'
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 
 
 # Import transforms in order to register classes for serialization
