12acee422855190353002be2be9fe862772caf0b
Daniele De Gregorio <degregorio.daniele@gmail.com>
2022-01-05 17:54:27 +0100

    New plain Stages example added

---
+++ examples/stages/README.md
@@ -0,0 +1,5 @@
+# How to load Stages from file and apply them to an Undefolder reader
+
+```
+python example_stages.py
+```
--- examples/stages/example_stages.py
+++ examples/stages/example_stages.py
@@ -1,14 +1,20 @@
 from choixe.spooks import Spook
-from choixe.configurations import XConfig
-
+import rich
 from pipelime.sequences.readers.filesystem import UnderfolderReader
 
-stage = Spook.create(XConfig("stages.yml"))
-print(stage)
+# load stages from file
+stage = Spook.create_from_file("stages.yml")
 
-
+# Input dataset
 dataset = UnderfolderReader("../data/underfolder/example_dataset")
-# dataset.stage = stage
 
+# Before stage
+rich.print("Before stage:")
 for sample in dataset:
-    print(list(sample.keys()))
+    rich.print(list(sample.keys()))
+
+# After stage
+rich.print("After stage:")
+dataset.stage = stage
+for sample in dataset:
+    rich.print(list(sample.keys()))
