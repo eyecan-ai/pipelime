fe3a5beefd54457967b4c5d9fedf1b48008d07fe
Daniele De Gregorio <degregorio.daniele@gmail.com>
2022-01-23 22:42:06 +0100

    Draw pipeline Graph with Diagrams

--- examples/proposal/piper/parse_piper_file.py
+++ examples/proposal/piper/parse_piper_file.py
@@ -1,3 +1,4 @@
+from black import os
 from choixe.configurations import XConfig
 import cv2
 import numpy as np
@@ -11,47 +12,100 @@ import string
 from faker import Faker
 from tempfile import NamedTemporaryFile
 import imageio
+from diagrams.custom import Custom
 
 
-def draw_graph(graph: nx.DiGraph, agraph: bool = True):
+class MyNode(Custom):
+    def __init__(self, label):
+        super().__init__(
+            label,
+            "/Users/danieledegregorio/work/workspace_eyecan/pipelime/examples/proposal/piper/resources/NodeUnderfolder.png",
+        )
+
+
+def draw_graph(graph: nx.DiGraph, agraph: bool = False):
 
     if agraph == False:
 
-        data_indices = []
-        op_indices = []
-        for index, n in enumerate(graph.nodes):
-            n: PipeGraph.PipeGraphNode
-            if n.type == "data":
-                data_indices.append(n)
-            elif n.type == "operation":
-                op_indices.append(n)
+        from diagrams import Diagram, Cluster
+        from diagrams.aws.compute import EC2
+        from diagrams.aws.compute import EC2
+        from diagrams.aws.database import RDS
+        from diagrams.aws.network import ELB
+        from diagrams.programming.language import Python as OperationNode
 
-        nx.draw_networkx_nodes(
-            graph,
-            pos,
-            nodelist=op_indices,
-            node_size=800,
-            node_shape="s",
-            node_color="#ff00aa",
-        )
-        nx.draw_networkx_nodes(
-            graph,
-            pos,
-            nodelist=data_indices,
-            node_size=500,
-            node_shape="o",
-            node_color="#ff0000",
-            label="data",
-        )
-        nx.draw_networkx_labels(graph, pos, font_size=12, font_family="sans-serif")
-        nx.draw_networkx_edges(graph, pos)
-        # nx.draw(graph, pos, with_labels=True)
-        f = NamedTemporaryFile(suffix=".png")
-        rich.print("Writing to", f.name)
-        plt.savefig(f.name, dpi=1000)
-        img = imageio.imread(f.name)
-        f.close()
+        f = NamedTemporaryFile(suffix="")
+
+        with Diagram(
+            "Pipeline", show=False, direction="TB", filename=f.name, outformat="png"
+        ):
+
+            execution_stack = graph.build_execution_stack()
+
+            nodes_map = {}
+
+            for layer_index, layer in enumerate(execution_stack):
+                with Cluster("Layer {}".format(layer_index)):
+                    for node in layer:
+                        node: PipeGraph.PipeGraphNode
+                        print("ADDING", node, type(node), node.id)
+                        if node.type == "operation":
+                            nodes_map[node.id] = OperationNode(node.name)
+                        else:
+                            nodes_map[node.id] = MyNode(node.name)
+
+            for node in graph.nodes():
+                node: PipeGraph.PipeGraphNode
+                if node.type == "operation":
+                    pass
+                else:
+                    nodes_map[node.id] = MyNode(node.name)
+
+            for edge in graph.edges():
+                n0: PipeGraph.PipeGraphNode = edge[0]
+                n1: PipeGraph.PipeGraphNode = edge[1]
+                nodes_map[n0.id] >> nodes_map[n1.id]
+
+            print("WRITTEN", f"{f.name}.png")
+        img = imageio.imread(f"{f.name}.png")
         return img
+        # os.system(f"open {f.name}")
+        # f.close()
+        # data_indices = []
+        # op_indices = []
+        # for index, n in enumerate(graph.nodes):
+        #     n: PipeGraph.PipeGraphNode
+        #     if n.type == "data":
+        #         data_indices.append(n)
+        #     elif n.type == "operation":
+        #         op_indices.append(n)
+
+        # nx.draw_networkx_nodes(
+        #     graph,
+        #     pos,
+        #     nodelist=op_indices,
+        #     node_size=800,
+        #     node_shape="s",
+        #     node_color="#ff00aa",
+        # )
+        # nx.draw_networkx_nodes(
+        #     graph,
+        #     pos,
+        #     nodelist=data_indices,
+        #     node_size=500,
+        #     node_shape="o",
+        #     node_color="#ff0000",
+        #     label="data",
+        # )
+        # nx.draw_networkx_labels(graph, pos, font_size=12, font_family="sans-serif")
+        # nx.draw_networkx_edges(graph, pos)
+        # # nx.draw(graph, pos, with_labels=True)
+        # f = NamedTemporaryFile(suffix=".png")
+        # rich.print("Writing to", f.name)
+        # plt.savefig(f.name, dpi=1000)
+        # img = imageio.imread(f.name)
+        # f.close()
+        # return img
     else:
 
         agraph = to_agraph(graph)
@@ -81,13 +135,12 @@ def draw_graph(graph: nx.DiGraph, agraph: bool = True):
 
 fake = Faker()
 
-splits_percentages = np.random.uniform(0.01, 0.99, size=(3,))
-splits_percentages = np.exp(splits_percentages) / np.sum(np.exp(splits_percentages))
-splits_name = [fake.name() for x in splits_percentages]
+splits_name = ["train", "validation", "test"]
+splits_percentages = [0.9, 0.08, 0.02]
 
 global_data = {
     "params": {
-        "input_folders": [fake.name() for _ in range(3)],
+        "input_folders": ["data0", "data1", "data2"],
         "converted_folder": "/tmp/converted",
         "summed_folder": "/home/summed",
         "detected_folder": "/home/detected",
@@ -129,5 +182,5 @@ pos = nx.nx_pydot.graphviz_layout(graph, prog="dot")
 #     rich.print(n)
 
 img = draw_graph(graph)
-cv2.imshow("graph", img)
+cv2.imshow("graph", cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
 cv2.waitKey(0)
