f21b29fed22a59476bf2b89c3c85ceaff9f0dd02
nfioraio-ec <nicola.fioraio@eyecan.ai>
2022-01-29 03:33:42 +0100

    Remote factory

--- pipelime/filesystem/remotes.py
+++ pipelime/filesystem/remotes.py
@@ -1,44 +1,88 @@
-from abc import ABC, abstractmethod
-from pathlib import Path
-from io import RawIOBase, BufferedIOBase
-from typing import Union, Optional
+from abc import ABCMeta, abstractmethod
+from pathlib import Path, WindowsPath
+from typing import Tuple, Union, Optional, BinaryIO
+
+from urllib.parse import urlparse, unquote_plus, ParseResult
+
+import os
+import shutil
 
 from loguru import logger
 
 
-class BaseRemote(ABC):
-    def __init__(self, hash_gen=None):
+class RemoteRegister(ABCMeta):
+    REMOTE_CLASSES = {}
+    REMOTE_INSTANCES = {}
+
+    def __init__(cls, name, bases, dct):
+        cls.REMOTE_CLASSES[cls.scheme()] = cls  # type: ignore
+        super().__init__(name, bases, dct)
+
+    @classmethod
+    def get_instance(cls, scheme: str, netloc: str):
+        remote_instance = cls.REMOTE_INSTANCES.get((scheme, netloc))
+        if remote_instance is None:
+            remote_class = cls.REMOTE_CLASSES.get(scheme)
+            if remote_class is not None:
+                remote_instance = remote_class(netloc)
+                cls.REMOTE_INSTANCES[(scheme, netloc)] = remote_instance
+            else:
+                logger.warning(f"Unknown remote scheme {scheme}.")
+        return remote_instance
+
+
+def create_remote_from_url(
+    url: str,
+) -> Tuple[Optional["BaseRemote"], Optional[str], Optional[str]]:
+    parsed_url = urlparse(url)
+
+    if len(parsed_url.path) > 1:
+        file_full_path = Path(unquote_plus(parsed_url.path)[1:])
+        file_path, file_name = file_full_path.parent, file_full_path.name
+    else:
+        file_path, file_name = None, None
+
+    return (
+        RemoteRegister.get_instance(parsed_url.scheme, parsed_url.netloc),
+        str(file_path),
+        str(file_name),
+    )
+
+
+class BaseRemote(metaclass=RemoteRegister):  # type: ignore
+    def __init__(self, netloc: str, hash_gen=None):
         import hashlib
 
         self._hash_gen = hashlib.sha256 if hash_gen is None else hash_gen
+        self._netloc = netloc
 
     def upload_file(
-        self, file_path: Union[Path, str], target_prefix: str
+        self, file_path: Union[Path, str], target_path: str
     ) -> Optional[str]:
         file_path = Path(file_path)
         file_size = file_path.stat().st_size
         with open(file_path, "rb") as file_data:
             return self.upload_stream(
-                file_data, file_size, target_prefix, "".join(file_path.suffixes)
+                file_data, file_size, target_path, "".join(file_path.suffixes)
             )
 
     def upload_stream(
         self,
-        stream: Union[RawIOBase, BufferedIOBase],
+        stream: BinaryIO,
         stream_size: int,
-        target_prefix: str,
+        target_path: str,
         target_suffix: str,
     ) -> Optional[str]:
         hash_name = self._compute_hash(stream)
         target_name = hash_name + target_suffix
-        if self._upload(stream, stream_size, target_prefix, target_name):
-            return self._make_url(target_prefix, target_name)
+        if self._upload(stream, stream_size, target_path, target_name):
+            return self._make_url(f"{target_path}/{target_name}")
         return None
 
     def download_file(
         self,
         file_path: Union[Path, str],
-        source_prefix: str,
+        source_path: str,
         source_name: str,
     ) -> bool:
         file_path = Path(file_path)
@@ -58,7 +102,7 @@ class BaseRemote(ABC):
 
         ok = False
         with open(part_file, "ab") as part_stream:
-            ok = self.download_stream(part_stream, source_prefix, source_name, offset)
+            ok = self.download_stream(part_stream, source_path, source_name, offset)
 
         if ok:
             file_path.unlink(missing_ok=True)
@@ -68,55 +112,64 @@ class BaseRemote(ABC):
 
     def download_stream(
         self,
-        stream: Union[RawIOBase, BufferedIOBase],
-        source_prefix: str,
+        stream: BinaryIO,
+        source_path: str,
         source_name: str,
         source_offset: int = 0,
     ) -> bool:
-        return self._download(stream, source_prefix, source_name, source_offset)
+        return self._download(stream, source_path, source_name, source_offset)
 
-    def _compute_hash(self, stream: Union[RawIOBase, BufferedIOBase]) -> str:
+    def _compute_hash(self, stream: BinaryIO) -> str:
         h = self._hash_gen()
         b = bytearray(1024 * 1024)
         mv = memoryview(b)
         fpos = stream.tell()
-        for n in iter(lambda: stream.readinto(mv), 0):
+        for n in iter(lambda: stream.readinto(mv), 0):  # type: ignore
             h.update(mv[:n])
         stream.seek(fpos)
         return h.hexdigest()
 
-    def _make_url(self, target_prefix: str, target_name: str):
-        return (
-            f"{self.protocol_code}://{self.endpoint_url}/{target_prefix}/{target_name}"
-        )
+    def _make_url(self, target_path: str):
+        return ParseResult(
+            scheme=self.scheme(),
+            netloc=self.netloc,
+            path=target_path,
+            params="",
+            query="",
+            fragment="",
+        ).geturl()
 
     @abstractmethod
     def _upload(
         self,
-        source: Union[RawIOBase, BufferedIOBase],
+        source: BinaryIO,
         source_size: int,
-        target_prefix: str,
+        target_path: str,
         target_name: str,
     ) -> bool:
         ...
 
     def _download(
         self,
-        target: Union[RawIOBase, BufferedIOBase],
-        source_prefix: str,
+        target: BinaryIO,
+        source_path: str,
         source_name: str,
         source_offset: int,
     ) -> bool:
         ...
 
-    @property
+    @classmethod
     @abstractmethod
-    def protocol_code(self) -> str:
+    def scheme(cls) -> str:
         pass
 
     @property
+    def netloc(self) -> str:
+        return self._netloc
+
+    @property
     @abstractmethod
-    def endpoint_url(self) -> str:
+    def is_valid(self) -> bool:
         pass
 
 
@@ -131,20 +184,16 @@ class S3Remote(BaseRemote):
         region: Optional[str] = None,
         hash_gen=None,
     ):
+        super().__init__(endpoint, hash_gen)
+
         try:
             from minio import Minio
             from minio.credentials import (
                 ChainedProvider,
-                # MINIO_ACCESS_KEY, MINIO_SECRET_KEY
-                EnvMinioProvider,
-                # AWS_ACCESS_KEY_ID|AWS_ACCESS_KEY,
-                # AWS_SECRET_ACCESS_KEY|AWS_SECRET_KEY,
-                # AWS_SESSION_TOKEN
                 EnvAWSProvider,
-                # ~/[.]mc/config.json
-                MinioClientConfigProvider,
-                # ~/.aws/credentials
+                EnvMinioProvider,
                 AWSConfigProvider,
+                MinioClientConfigProvider,
             )
 
             self._client = Minio(
@@ -156,74 +205,167 @@ class S3Remote(BaseRemote):
                 region=region,
                 credentials=ChainedProvider(
                     [
-                        EnvMinioProvider(),
+                        # AWS_ACCESS_KEY_ID|AWS_ACCESS_KEY,
+                        # AWS_SECRET_ACCESS_KEY|AWS_SECRET_KEY,
+                        # AWS_SESSION_TOKEN
                         EnvAWSProvider(),
-                        MinioClientConfigProvider(),
+                        # MINIO_ACCESS_KEY, MINIO_SECRET_KEY
+                        EnvMinioProvider(),
+                        # ~/.aws/credentials
                         AWSConfigProvider(),
+                        # ~/[.]mc/config.json
+                        MinioClientConfigProvider(),
                     ]
                 ),
             )
-            self._endpoint = endpoint
         except ModuleNotFoundError:  # pragma: no cover
-            logger.error("S3 remote needs `minio` python package.")  # pragma: no cover
+            logger.error("S3 remote needs `minio` python package.")
+            self._client = None
 
     def _upload(
         self,
-        source: Union[RawIOBase, BufferedIOBase],
+        source: BinaryIO,
         source_size: int,
-        target_prefix: str,
+        target_path: str,
         target_name: str,
     ) -> bool:
-        try:
-            if not self._client.bucket_exists(target_prefix):
-                logger.info(
-                    f"Creating bucket {target_prefix} on S3 remote {self.endpoint_url}"
-                )
-                self._client.make_bucket(target_prefix)
-            self._client.put_object(
-                bucket_name=target_prefix,
-                object_name=target_name,
-                data=source,
-                length=source_size,
-            )
-        except Exception as exc:
-            logger.warning(str(exc))
-            return False
+        if self.is_valid:
+            try:
+                if not self._client.bucket_exists(target_path):  # type: ignore
+                    logger.info(
+                        f"Creating bucket {target_path} on S3 remote {self.netloc}."
+                    )
+                    self._client.make_bucket(target_path)  # type: ignore
 
-        return True
+                self._client.put_object(  # type: ignore
+                    bucket_name=target_path,
+                    object_name=target_name,
+                    data=source,
+                    length=source_size,
+                )
+                return True
+            except Exception as exc:
+                logger.warning(str(exc))
+                return False
+
+        return False
 
     def _download(
         self,
-        target: Union[RawIOBase, BufferedIOBase],
-        source_prefix: str,
+        target: BinaryIO,
+        source_path: str,
         source_name: str,
         source_offset: int,
     ) -> bool:
-        if not self._client.bucket_exists(source_prefix):
-            logger.info(
-                f"Bucket {source_prefix} does not exist "
-                f"on S3 remote {self.endpoint_url}"
-            )
-            return False
+        if self.is_valid:
+            if not self._client.bucket_exists(source_path):  # type: ignore
+                logger.warning(
+                    f"Bucket {source_path} does not exist "
+                    f"on S3 remote {self.netloc}."
+                )
+                return False
 
-        response = None
-        try:
-            response = self._client.get_object(
-                bucket_name=source_prefix, object_name=source_name, offset=source_offset
-            )
-            for data in response.stream(amt=1024 * 1024):
-                target.write(data)
-        finally:
-            if response:
-                response.close()
-                response.release_conn()
-                return True
+            response = None
+            ok = False
+            try:
+                response = self._client.get_object(  # type: ignore
+                    bucket_name=source_path,
+                    object_name=source_name,
+                    offset=source_offset,
+                )
+                for data in response.stream(amt=1024 * 1024):
+                    target.write(data)
+                ok = True
+            except Exception as exc:
+                logger.warning(str(exc))
+                return False
+            finally:
+                if response:
+                    response.close()
+                    response.release_conn()
+                return ok
+
         return False
 
-    @property
-    def protocol_code(self) -> str:
+    @classmethod
+    def scheme(cls) -> str:
         return "s3"
 
     @property
-    def endpoint_url(self) -> str:
-        return self._endpoint
+    def is_valid(self) -> bool:
+        return self._client is not None
+
+
+class FileRemote(BaseRemote):
+    def __init__(self, netloc: str, hash_gen=None):
+        if netloc == "localhost" or netloc == "127.0.0.1":
+            netloc = ""
+        super().__init__(netloc, hash_gen)
+
+    def _upload(
+        self,
+        source: BinaryIO,
+        source_size: int,
+        target_path: str,
+        target_name: str,
+    ) -> bool:
+        if self.is_valid:
+            try:
+                full_target_path = self._make_file_path(target_path, target_name)
+                if not full_target_path.parent.exists():
+                    logger.info(f"Creating folder tree {full_target_path.parent}.")
+                    full_target_path.parent.mkdir(parents=True, exist_ok=True)
+
+                with full_target_path.open("wb") as target:
+                    shutil.copyfileobj(source, target)
+
+                return True
+            except Exception as exc:
+                logger.warning(str(exc))
+                return False
+
+        return False
+
+    def _download(
+        self,
+        target: BinaryIO,
+        source_path: str,
+        source_name: str,
+        source_offset: int,
+    ) -> bool:
+        if self.is_valid:
+            try:
+                full_source_path = self._make_file_path(source_path, source_name)
+                if not full_source_path.exists():
+                    logger.warning(f"File {full_source_path} does not exist.")
+                    return False
+
+                with full_source_path.open("rb") as source:
+                    source.seek(source_offset)
+                    shutil.copyfileobj(source, target)
+
+                return True
+            except Exception as exc:
+                logger.warning(str(exc))
+                return False
+
+        return False
+
+    def _make_file_path(self, file_path: str, file_name: str) -> Path:
+        full_path = Path(file_path) / Path(file_name)
+        if self.netloc:
+            return Path(
+                "{0}{0}{1}{0}{2}".format(os.path.sep, self.netloc, str(full_path))
+            )
+        elif isinstance(full_path, WindowsPath):
+            return full_path
+        else:
+            return Path(os.path.sep + str(full_path))
+
+    @classmethod
+    def scheme(cls) -> str:
+        return "file"
+
+    @property
+    def is_valid(self) -> bool:
+        return True
